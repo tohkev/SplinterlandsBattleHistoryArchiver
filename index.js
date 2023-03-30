0ce587c293f47d1dedfe15ca03257f0e83a43cb1
Kevin Toh <tohkev@gmail.com>
2023-03-29 22:41:41 -0700

    fixed issue where json files were being uploaded blank

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const Dropbox = require("dropbox").Dropbox;
-require('dotenv').config();
 
 const scheduler = require('./helper');
 const getBattleData = require("./getBattleData");
 
 function uploadToDropbox(){
-    const dataArray = getBattleData();
-    const jsonString = JSON.stringify(dataArray);
-    const accessToken = process.env.ACCESS_TOKEN;
-
-    const dbx = new Dropbox({accessToken: accessToken})
-
-    const now = new Date();
-
-    dbx.filesUpload({
-        path: `/history${now.getMonth()+1}${now.getDate()}.json`,
-        contents: Buffer.from(jsonString),
-        mode: {'.tag': 'overwrite'},
-        autorename: true,
-        mute: false,
-    }).then((response) => {
-        console.log('File uploaded', response);
-    }).catch((error) => {
-        console.error('Error uploading file:', error);
+    getBattleData().then((data) => {
+        const jsonString = JSON.stringify(data);
+        const accessToken = process.env.ACCESS_TOKEN;
+    
+        const dbx = new Dropbox({accessToken: accessToken})
+    
+        const now = new Date();
+    
+        dbx.filesUpload({
+            path: `/history${now.getMonth()+1}${now.getDate()}.json`,
+            contents: jsonString,
+            mode: {'.tag': 'overwrite'},
+            autorename: true,
+            mute: false,
+        }).then((response) => {
+            console.log('File uploaded', response);
+        }).catch((error) => {
+            console.error('Error uploading file:', error);
+        });
     });
 }
 
